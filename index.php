e9976164069d9ff7eedb3f3dd34a0e1e8e8bd10c
Nikola <nikolabojkovic@gmail.com>
2015-02-28 16:54:00 +0100

    [FW-0001] folder structure

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>INDEX</title>
-	</head>
-	<body>
-		Hello
-	</body>
-</html>
+<?php require_once (dirname($_SERVER['REQUEST_URI']).'views/header.php');?>
+	<p>Hello</p>
+<?php require_once (dirname($_SERVER['REQUEST_URI']).'views/footer.php');?>
---
+++ views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>INDEX</title>
+		<meta charset="utf-8">
+		<link href="<?php echo(substr(dirname($_SERVER['REQUEST_URI']).'css/bootstrap-3.1.1-dist/css/bootstrap.min.css',1)); ?>" rel="stylesheet">
+		<link href="<?php echo(substr(dirname($_SERVER['REQUEST_URI']).'css/header.css',1)); ?>" rel="stylesheet" type="text/css">
+		<script src="<?php echo(substr(dirname($_SERVER['REQUEST_URI']).'js/jquery-1.11.1.min.js',1)); ?>"></script>		
+	</head>
+	<body>
+	<header>
+		header
+	</header>
